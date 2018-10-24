e886c38ca0f7018e3ff3a22900931f65d4a87b95
S Arun sekaran <arunsekaran02@gmail.com>
2018-10-24 18:55:01 +0530

    1.removed SunTrust name in java docs 2.setFragment logic moved to BaseActivity

--- app/src/main/java/com/testing/sample/ui/todo/adapter/TodoViewHolder.kt
+++ app/src/main/java/com/testing/sample/ui/todo/adapter/TodoViewHolder.kt
@@ -4,9 +4,6 @@ import com.testing.sample.Todo
 import com.testing.sample.base.BaseViewHolder
 import com.testing.sample.databinding.ViewHolderTodoBinding
 
-/**
- * Created by SunTrust on 10/18/2018.
- */
 class TodoViewHolder(private val binding: ViewHolderTodoBinding) : BaseViewHolder<Todo>(binding.root) {
     override fun onBind(todo: Todo?) {
         todo?.let {
